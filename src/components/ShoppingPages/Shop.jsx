156c91268adac05d151c61c050c8c655108b25fc
Ben <baguirre@luc.edu>
2023-10-16 14:18:51 -0500

    add Icons

--- src/components/ShoppingPages/Shop.jsx
+++ src/components/ShoppingPages/Shop.jsx
@@ -5,12 +5,14 @@ import store1 from "../../assets/store-img.png";
 import store2 from "../../assets/store-img1.png";
 import store3 from "../../assets/store-img2.png";
 import store4 from "../../assets/store-img3.png";
-import { Link } from "react-router-dom";
-
 import trending from "../../assets/store.png";
 import women from "../../assets/store1.png";
 import men from "../../assets/store2.png";
 import kid from "../../assets/store3.png";
+
+import { Link } from "react-router-dom";
+import ProfileCart from "../HelperComponents/ProfileCart";
+import SearchInput from "../HelperComponents/SearchInput";
 
 const deals = [
   { name: "Sale" },
@@ -62,31 +64,19 @@ export default function Shop() {
           </h2>
         </Link>
         <div className="inputs-shop">
-          <input placeholder="16 E Pearson St, Chicago, IL 60611"></input>
-          <input placeholder="Search stores, products, etc"></input>
-        </div>
-        <div className="buttons-shop">
-          <button className="header-button">
-            <svg
-              className="profile icon"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <title>account-circle</title>
-              <path d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z" />
+          <div className="shop-search-container">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <title>map-marker</title>
+              <path d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" />
             </svg>
-          </button>
-          <button className="header-button">
-            <svg
-              className="cart icon"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <title>cart</title>
-              <path d="M17,18C15.89,18 15,18.89 15,20A2,2 0 0,0 17,22A2,2 0 0,0 19,20C19,18.89 18.1,18 17,18M1,2V4H3L6.6,11.59L5.24,14.04C5.09,14.32 5,14.65 5,15A2,2 0 0,0 7,17H19V15H7.42A0.25,0.25 0 0,1 7.17,14.75C7.17,14.7 7.18,14.66 7.2,14.63L8.1,13H15.55C16.3,13 16.96,12.58 17.3,11.97L20.88,5.5C20.95,5.34 21,5.17 21,5A1,1 0 0,0 20,4H5.21L4.27,2M7,18C5.89,18 5,18.89 5,20A2,2 0 0,0 7,22A2,2 0 0,0 9,20C9,18.89 8.1,18 7,18Z" />
-            </svg>
-          </button>
+            <input
+              className="shop-search"
+              placeholder="16 E Pearson St, Chicago, IL 60611"
+            ></input>
+          </div>
+          <SearchInput></SearchInput>
         </div>
+        <ProfileCart></ProfileCart>
       </div>
       <div className="categories">
         {categories.map((deal) => {
